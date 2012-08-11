67f9881f329c4d539b6be1ef2e7cd0f08e4a9cdd
Michael Writhe <michael@writhem.com>
2012-08-10 21:27:44 -0600

    added VERY basic admin framework, as per agrif request

---
+++ admin.py
@@ -0,0 +1,8 @@
+from models import SongFile
+from models import UserSong
+from models import GlobalSong
+from django.contrib import admin
+
+admin.site.register(SongFile)
+admin.site.register(UserSong)
+admin.site.register(GlobalSong)
