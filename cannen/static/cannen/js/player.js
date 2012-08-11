f0ba357ad1c19822576ed47349c816e8ad350604
Aaron Griffith <aargri@gmail.com>
2012-08-11 00:31:13 -0400

    added fancy javascript file uploader

--- cannen/static/cannen/js/player.js
+++ cannen/static/cannen/js/player.js
@@ -1,3 +1,4 @@
+// helper to refresh the info asynchronously
 function refresh()
 {
 	$('#info').load('info',
@@ -26,6 +27,7 @@ function refresh()
 $(document).ready(
 	function (event)
 	{
+		// refresh info often
 		refresh();
 		var auto_refresh = setInterval(refresh, 2500); // every 2.5 seconds
 		
@@ -44,6 +46,48 @@ $(document).ready(
 					   }
 				);
 			}
-		);		
+		);
+		
+		// set up the AJAX-ey file uploader
+		$('#fileupload').fileupload(
+			{
+				dataType: 'txt',
+				sequentialUploads: true,
+				
+				submit: function (e, data)
+				{
+					data.context = add_upload();
+					if (data.context.name)
+						data.context.name.text(data.files[0].name);
+					if (data.context.status)
+						data.context.status.text('queued');
+					return true;
+				},
+				send: function (e, data)
+				{
+					if (data.context.status)
+						data.context.status.text('0%');
+				},
+				progress: function (e, data)
+				{
+					var progress = parseInt(data.loaded / data.total * 100, 10);
+					if (data.context.progress)
+						data.context.progress.animate({width: progress + '%'});
+					if (data.context.status)
+						data.context.status.text(progress + '%');
+				},
+				always: function (e, data)
+				{
+					if (data.context.status)
+						data.context.status.text('done');
+					if (data.context.main)
+						data.context.main.fadeOut(1000, function() { $(this).remove(); });
+					refresh();
+				},
+			}
+		);
+		
+		// since we have javascript, hide the submit button for files
+		$("#fileuploadsubmit").remove();
 	}
 );
